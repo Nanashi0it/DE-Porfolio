2e2091bd6ca77028d012c96340d509fda9e52952
Hoang Manh Khiem <hoangmanhkhiem2001@gmail.com>
2023-12-06 20:22:02 +0700

    Init DE-Porfolio

---
+++ script.js
@@ -0,0 +1,32 @@
+function dragScroll(idName) {
+    let mouseDown = false;
+    let startX, scrollLeft;
+    const slider = document.getElementById(idName);
+    
+    const startDragging = (e) => {
+      mouseDown = true;
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    }
+    
+    const stopDragging = (e) => {
+      mouseDown = false;
+    }
+    
+    const move = (e) => {
+      e.preventDefault();
+      if(!mouseDown) { return; }
+      const x = e.pageX - slider.offsetLeft;
+      const scroll = x - startX;
+      slider.scrollLeft = scrollLeft - scroll;
+    }
+    
+    // Add the event listeners
+    slider.addEventListener('mousemove', move, false);
+    slider.addEventListener('mousedown', startDragging, false);
+    slider.addEventListener('mouseup', stopDragging, false);
+    slider.addEventListener('mouseleave', stopDragging, false);
+}
+
+dragScroll("drag-1")
+dragScroll("drag-2")
